8e3d798763361c98dc40b412efcf8000ea1ee4ca
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-05-09 22:41:40 -0500

    Agregar pagina de bienvenida

---
+++ indice.js
@@ -0,0 +1,5 @@
+let play = document.querySelector("#jugar");
+play.addEventListener("click",goToPlay);
+function goToPlay(){
+    location.href="generalities.html";
+};
--- main.js
+++ main.js
@@ -1,41 +1,47 @@
 //Muestra la alternativa correcta e incorrecta para la primera pregunta.
-let pruebaUno = document.querySelector('#boton_uno');
-pruebaUno.addEventListener("click",uno);
+let replyDay = document.querySelector('#boton_day');
+replyDay.addEventListener("click",selectDay);
 
-function uno(){
-    let seleccion = document.querySelector('input[type="radio"][name="dia_mundial"]:checked');
-    let correcta =document.querySelector('input[type="radio"][id="junio"]');
-    if (seleccion === correcta){
-        alert(`La alternativa correcta es:  ${correcta.value}. \n¡Tu respuesta es correcta!`);
+function selectDay(){
+    let day = document.querySelector('input[type="radio"][name="dia_mundial"]:checked');
+    let correctDay =document.querySelector('input[type="radio"][id="junio"]');
+    if (day === correctDay){
+        alert(`La alternativa correcta es:  ${correctDay.value}. \n¡Tu respuesta es correcta!`);
     }else{
-        alert(`La respuesta correcta es:  ${correcta.value}. \nTu respuesta es incorrecta.`);
+        alert(`La respuesta correcta es:  ${correctDay.value}. \nTu respuesta es incorrecta.`);
     }
 };
 
 //Muestra la alternativa correcta e incorrecta para la segunda pregunta.
-let pruebaDos = document.querySelector('#boton_dos');
-pruebaDos.addEventListener("click",dos);
+let replyOrg = document.querySelector('#boton_org');
+replyOrg.addEventListener("click",selectOrg);
 
-function dos(){
-    let seleccionDos = document.querySelector('input[type="radio"][name="organizacion"]:checked');
-    let correctaDos =document.querySelector('input[type="radio"][id="onu"]');
-    if (seleccionDos === correctaDos){
-        alert(`La alternativa correcta es:  ${correctaDos.value}. \n¡Tu respuesta es correcta!`);
+function selectOrg(){
+    let org = document.querySelector('input[type="radio"][name="organizacion"]:checked');
+    let correctOrg =document.querySelector('input[type="radio"][id="onu"]');
+    if (org === correctOrg){
+        alert(`La alternativa correcta es:  ${correctOrg.value}. \n¡Tu respuesta es correcta!`);
     }else{
-        alert(`La respuesta correcta es:  ${correctaDos.value}. \nTu respuesta es incorrecta.`);
+        alert(`La respuesta correcta es:  ${correctOrg.value}. \nTu respuesta es incorrecta.`);
     }
 };
 
 //Muestra la alternativa correcta e incorrecta para la tercera pregunta.
-let pruebaTres = document.querySelector('#boton_tres');
-pruebaTres.addEventListener("click",tres);
+let replyScience = document.querySelector('#boton_science');
+replyScience.addEventListener("click",selectScience);
 
-function tres(){
-    let seleccionTres = document.querySelector('input[type="radio"][name="ciencia"]:checked');
-    let correctaTres =document.querySelector('input[type="radio"][id="ecologia"]');
-    if (seleccionTres === correctaTres){
-        alert(`La alternativa correcta es:  ${correctaTres.value}. \n¡Tu respuesta es correcta!`);
+function selectScience(){
+    let science = document.querySelector('input[type="radio"][name="ciencia"]:checked');
+    let correctScience =document.querySelector('input[type="radio"][id="ecologia"]');
+    if (science === correctScience){
+        alert(`La alternativa correcta es:  ${correctScience.value}. \n¡Tu respuesta es correcta!`);
     }else{
-        alert(`La respuesta correcta es:  ${correctaTres.value}. \nTu respuesta es incorrecta.`);
+        alert(`La respuesta correcta es:  ${correctScience.value}. \nTu respuesta es incorrecta.`);
     }
+};
+
+let back = document.querySelector("#regresar");
+back.addEventListener("click",goToBack);
+function goToBack(){
+    location.href="index.html";
 };
